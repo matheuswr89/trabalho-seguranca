0d4b2175a6d8fe96e578bf55e20e1f73a266f225
Matheus William <matheuswr89@outlook.com>
2023-12-08 19:50:10 -0300

    feat: add password key input and adjust code

--- src/main.js
+++ src/main.js
@@ -6,99 +6,115 @@ const { usb } = require("usb");
 const { execSync } = require("child_process");
 const { app, BrowserWindow, ipcMain, dialog } = require("electron");
 
+let globalWindow = null;
+
+const resetValue = "document.getElementById('inputField').value = ''";
+const resetValuePassword = "document.getElementById('inputFieldPassword').value = ''";
+const formDisplayNone = "document.getElementById('form').style.display = \"none\"";
+const formDisplayBlock = "document.getElementById('form').style.display = \"block\"";
+const retryButtonDisplayNone = "document.getElementById('retryButton').style.display = \"none\"";
+const retryButtonDisplayBlock = "document.getElementById('retryButton').style.display = \"block\"";
+const devicesSendMessage = "document.getElementById('devices').textContent = 'Insira um usb com a chave privada!'";
+
 function getUnixDisks() {
     const dmesgResult = execSync("dmesg --notime | grep sd").toString().split("\n");
-    let splitResult = dmesgResult[dmesgResult.length - 4].trim().split(":")
-    if (splitResult.length !== 2) splitResult = dmesgResult[dmesgResult.length - 3].trim().split(":")
-    const lines = execSync(`lsblk -pl | grep${splitResult[1]} | awk '{print $7}'`).toString().replace("\n", "");
-
-    return lines;
+    let splitResult = dmesgResult[dmesgResult.length - 4].trim().split(":");
+    if (splitResult.length !== 2) splitResult = dmesgResult[dmesgResult.length - 3].trim().split(":");
+    const lines = execSync(`lsblk -pl | grep${splitResult[1]}`).toString().split(/ |\n/g);
+    return lines.filter(e => e).slice(6).join(" ");
 }
 
-async function readDir(disk, window) {
+async function readDir(disk) {
     try {
         const files = await fs.readdirSync(disk, { withFileTypes: true });
         const pemFile = files.filter(file => file.name.includes(".pem"))[0];
 
         if (pemFile) {
             const filePath = `${disk}/${pemFile.name}`;
-
             const certContent = await fs.readFileSync(filePath);
-            const encrypted = await encryptStringWithPrivateKey(certContent);
 
-            window.webContents.send("devices", "");
-            window.webContents.executeJavaScript("document.getElementById('form').style.display = \"block\"")
-            ipcMain.on('asynchronous-message', (event, arg) => {
-                if (arg) {
-                    window.webContents.executeJavaScript("document.getElementById('inputField').value = ''")
-                    window.webContents.executeJavaScript("document.getElementById('form').style.display = \"none\"")
-                    decryptStringWithPublicKey(encrypted, window, arg)
+            globalWindow.webContents.send("devices", "");
+            globalWindow.webContents.executeJavaScript(formDisplayBlock);
+            ipcMain.on('asynchronous-message', async (event, arg) => {
+                if (arg.name && arg.senha) {
+                    const encrypted = encryptStringWithPrivateKey(certContent, arg.senha);
+                    globalWindow.webContents.executeJavaScript(resetValue);
+                    globalWindow.webContents.executeJavaScript(resetValuePassword);
+                    globalWindow.webContents.executeJavaScript(formDisplayNone);
+                    if(encrypted) {
+                        decryptStringWithPublicKey(encrypted, arg.name);
+                    }
                 }
-                else dialog.showMessageBox(window, { message: "Forneça o seu nome!", type: "error" })
+                else dialog.showMessageBox(globalWindow, { message: "Forneça o seu nome e a senha da chave!", type: "error" });
             });
         } else {
-            window.webContents.send("devices", "Nenhuma chave privada foi encontrada.");
+            globalWindow.webContents.send("devices", "Nenhuma chave privada foi encontrada.");
         }
     } catch (err) {
-        window.webContents.send("devices", `Erro ao gerar o texto criptografado, verifique se o arquivo está corretamente formatado.`);
+        globalWindow.webContents.send("devices", `Erro ao gerar o texto criptografado.`);
     }
 }
 
-async function decryptStringWithPublicKey(toDecrypt, window, nomeUser) {
-    const pwd = execSync("pwd").toString().split("\n")[0];
-    const files = await fs.readdirSync(`${pwd}/src/chaves/`, { withFileTypes: true });
-    const file = files.filter(f => f.name.split(".")[0] === nomeUser)
-    if (file.length === 1) {
-        const chavePublica = await fs.readFileSync(`${file[0].path}/${file[0].name}`);
+async function decryptStringWithPublicKey(toDecrypt, nomeUser) {
+    const absolutePath = path.resolve(".");
+    const file = `${absolutePath}/src/chaves/pub-${nomeUser}.pem`;
+    if (fs.existsSync(file)) {
+        const chavePublica = await fs.readFileSync(file);
         try {
             const buffer = Buffer.from(toDecrypt, "base64");
-            const userName = new crypto.X509Certificate(chavePublica).issuer.split("\n")[4].replace("CN=", "");
+            const userName = new crypto.X509Certificate(chavePublica).issuer.match(/^CN.*$/gm)[0].replace("CN=", "");
             const decrypted = crypto.publicDecrypt(chavePublica, buffer);
-            const displayContent = `${decrypted.toString("utf-8")} Meu nome é ${userName}!`
-            window.webContents.send("devices", displayContent);
+            globalWindow.webContents.send("devices", `${decrypted.toString("utf-8")} Seu nome é ${userName}!`);
         } catch (err) {
-            console.log(err)
-            window.webContents.send("devices", "Acesso negado!");
+            globalWindow.webContents.send("devices", "Acesso negado! Verifique os dados e tente novamente!");
+            globalWindow.webContents.executeJavaScript(retryButtonDisplayBlock);
         }
     } else {
-        window.webContents.send("devices", "Você não está na lista de autorizados!");
+        globalWindow.webContents.send("devices", "Você não está na lista de autorizados!");
+        globalWindow.webContents.executeJavaScript(retryButtonDisplayBlock);
     }
 };
 
-function encryptStringWithPrivateKey(privateKey) {
-    const buffer = Buffer.from("Olá mundo!");
-    const encrypted = crypto.privateEncrypt(privateKey, buffer);
-    return encrypted.toString("base64");
+function encryptStringWithPrivateKey(key, passphrase) {
+    try {
+        const buffer = Buffer.from("Olá mundo!");
+        const encrypted = crypto.privateEncrypt({ key, passphrase }, buffer);
+        return encrypted.toString("base64");
+    } catch (err) { 
+        globalWindow.webContents.executeJavaScript(retryButtonDisplayBlock);
+        globalWindow.webContents.send("devices", 'Acesso negado! Verifique os dados e tente novamente!');
+    }
 };
 
-async function showDevices(window) {
-    window.webContents.executeJavaScript("document.getElementById('form').style.display = \"none\"");
-    usb.on("attach", async function (device) {
-        window.webContents.send("devices", "Carregando...");
+async function showDevices() {
+    globalWindow.webContents.executeJavaScript(formDisplayNone);
+    globalWindow.webContents.executeJavaScript(retryButtonDisplayNone);
+    usb.on("attach", async function () {
+        globalWindow.webContents.send("devices", "Carregando...");
         await new Promise((resolve) => setTimeout(resolve, 3000));
         const disk = getUnixDisks();
-        readDir(disk, window);
+        readDir(disk);
     });
 
-    usb.on("detach", async function (device) {
-        window.webContents.send("devices", "Insira um usb com a chave privada!");
+    usb.on("detach", async function () {
+        globalWindow.webContents.send("devices", "Insira um usb com a chave privada!");
     });
 };
 
 function createWindow() {
     const window = new BrowserWindow({
         width: 300,
-        height: 120,
+        height: 160,
         webPreferences: {
             preload: path.join(__dirname, "preload.js"),
             nodeIntegration: true,
             contextIsolation: false,
         }
     });
-
-    window.loadFile("index.html");
-    window.webContents.executeJavaScript("document.getElementById('devices').textContent = 'Insira um usb com a chave privada!'");
-    showDevices(window);
+    globalWindow = window;
+    globalWindow.loadFile("index.html");
+    globalWindow.webContents.executeJavaScript(devicesSendMessage);
+    showDevices();
 };
 
 app.whenReady().then(() => {
--- src/script.js
+++ src/script.js
@@ -1,8 +1,23 @@
 const { ipcRenderer } = require('electron')
 
-document.getElementById("myButton").addEventListener("click", myFunction);
+document.getElementById("sendButton").addEventListener("click", readValues);
+document.getElementById("retryButton").addEventListener("click", retryForm);
+window.addEventListener("keyup", eventHandler, false);
 
-function myFunction() {
-  var inputValue = document.getElementById('inputField').value;
-  ipcRenderer.send('asynchronous-message', inputValue)
+function retryForm() {
+  document.getElementById('form').style.display = "block"
+  document.getElementById('retryButton').style.display = "none"
+  document.getElementById('devices').innerHTML = ""
+}
+
+function readValues() {
+  const inputValue = document.getElementById('inputField').value;
+  const inputValuePassword = document.getElementById('inputFieldPassword').value;
+  ipcRenderer.send('asynchronous-message', { name: inputValue, senha: inputValuePassword })
+}
+
+function eventHandler(e) {
+  if (e.key === "Enter" && document.getElementById('form').style.display !== "none") {
+    readValues();
+  }
 }
